b38dd7d8e06022745af14c6f5055831be0b02de9
CleoBarlas <123100973+CleoBarlas@users.noreply.github.com>
2023-02-01 16:51:33 -0500

    Update JavaApplication1.java

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -6,7 +6,10 @@ import java.util.Scanner;
  *
  * @author klengs
  */
-/* added comment for fetch+merge
+/* added comment for fetch+merge*/
+/*added comment for pull*/
+
+
 public class JavaApplication1 {
 
     /**
