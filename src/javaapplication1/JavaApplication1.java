e86123ffe5cc8a1d1ebb5331ea80eb0ce7b955e1
klengs <klengs@localhost>
2023-02-01 15:27:59 -0500

    first commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,17 @@
+
+package javaapplication1;
+
+/**
+ *
+ * @author klengs
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
